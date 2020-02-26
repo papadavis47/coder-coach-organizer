a75d136d99cd4fb9a5aea272e5688280f63df649
John William Davis <jwdavisdeveloper@gmail.com>
2020-02-26 12:51:50 -0800

    made solid routes in topics controller file

--- controllers/topics.js
+++ controllers/topics.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // This will display all current topics for a user
 
 router.get("/", (req, res) => {
-  res.render("topics/show.ejs");
+  res.render("topics/index.ejs");
 });
 
 // This will contain the form for  new topic to be created
@@ -14,23 +14,12 @@ router.get("/new", (req, res) => {
   res.render("topics/new.ejs");
 });
 
-// This will display a particular topic and any notes that are connected to it
+// This will display a particular topic 
 
-router.get("/:id/notes", (req, res) => {
-  res.render("topics/notes.ejs");
-});
-
-// This will display a particular topic and any links that are connected to it
-
-router.get("/:id/links", (req, res) => {
-  res.render("topics/links");
-});
-
-// This will display a particular topic and any images that are connected to it
+router.get("/:id", (req, res) => {
+  res.render("topics/show.ejs");
+})
 
 
-router.get("/:id/images", (req, res) => {
-  res.render("topics/images");
-});
 
 module.exports = router;
--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next) {
     // check if there is not a user
     if (!req.user) {
-        req.flash('error', 'Yuu must be logged in to access this page')
+        req.flash('error', 'You must be logged in to access this page')
         res.redirect('/auth/login');
     } else {
         next();
